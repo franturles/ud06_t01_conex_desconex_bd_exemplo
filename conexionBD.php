0879b8df6f27b0d54c008306ed2f7000da2f2a90
franturles <kikofran35@hotmail.com>
2022-03-03 12:05:31 +0000

    Terminado

---
+++ conexionBD.php
@@ -0,0 +1,27 @@
+<?php
+
+	//Conexión
+	$conexionBD = @mysqli_connect('localhost', 'Franlf', 'abc123.');
+	if (!$conexionBD) {
+
+		echo('Erro número ' . mysqli_connect_errno() . ' ao establecer a conexión: ' . mysqli_connect_error() . '.');
+
+	} else {
+
+		echo('Conexión establecida con éxito<br/>');
+		//Selección da BD
+		if (!mysqli_select_db($conexionBD, 'IAW')) {
+			echo('Erro número ' . mysqli_errno($conexionBD) . ' ao seleccionar a BD: ' . mysqli_error($conexionBD) . '.');
+
+		} else {
+
+			echo('Selección da BD realizada con éxito.<br/>');
+		}
+		if (!@mysqli_close($conexionBD)) {
+			echo('Erro número ' . mysqli_errno($conexionBD) . ' ao pechar a conexión: ' . mysqli_error($conexionBD) . '.');
+		} else {
+			echo('Conexión pechada con éxito');
+		}
+	}
+
+?>
